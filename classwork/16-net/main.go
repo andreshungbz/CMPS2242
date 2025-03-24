bfd58b2c2db5a98f3a2af444061b075a529639be
Andres Hung <2018118240@ub.edu.bz>
2025-03-24 13:41:14 -0600

    Make port scanner

--- classwork/16-net/main.go
+++ classwork/16-net/main.go
@@ -2,26 +2,61 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
+	"strconv"
+	"sync"
 	"time"
 )
 
 func main() {
 	target := "scanme.nmap.org"
-	port := 80
-	address := net.JoinHostPort(target, fmt.Sprintf("%d", port))
+	// port := 80
+	// portStr := strconv.Itoa(port)
+	// address := net.JoinHostPort(target, portStr)
 
 	// set a timeout
 	dialer := net.Dialer{
 		Timeout: 5 * time.Second,
 	}
 
-	conn, err := dialer.Dial("tcp", address)
-	if err != nil {
-		log.Fatalf("unable to connect to %s: %v", address, err)
-	}
-	defer conn.Close()
+	maxRetries := 3
 
-	fmt.Printf("connection to %s was successful\n", address)
+	// ONE CONNECTION
+	// conn, err := dialer.Dial("tcp", address)
+	// if err != nil {
+	// 	log.Fatalf("unable to connect to %s: %v", address, err)
+	// }
+	// defer conn.Close()
+
+	// fmt.Printf("connection to %s was successful\n", address)
+
+	var wg sync.WaitGroup
+
+	for port := 80; port <= 85; port++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			address := net.JoinHostPort(target, strconv.Itoa(port))
+
+			fmt.Printf("[PORT %d - %s]\n", port, address)
+
+			for i := 0; i < maxRetries; i++ {
+				conn, err := dialer.Dial("tcp", address)
+				if err == nil {
+					defer conn.Close()
+					fmt.Printf("[SUCCESS] %s\n", address)
+					break
+				}
+
+				backoff := time.Duration(1<<i) * time.Second
+				fmt.Printf("[%d] Attempt %d failed. Waiting %v...\n", port, i+1, backoff)
+				time.Sleep(backoff)
+			}
+		}()
+
+	}
+
+	wg.Wait()
 }
